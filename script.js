f83981d79d52ab1e57777950a9041dfa4f91f287
Chris Pierce <crizpiz@gmail.com>
2015-07-23 04:58:34 -0500

    adding the 3 files

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+            $('div').mouseenter(function() {
+            $('div').fadeTo("fast", 1);
+        });
+
+});
+
+$(document).ready(function(){
+            $('div').mouseleave(function() {
+            $('div').fadeTo("fast", 0.5);
+        });
+
+});
